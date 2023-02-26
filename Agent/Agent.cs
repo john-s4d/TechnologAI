baa56cc194c8a4d4d4b3a0afc307ab029936ad8b
ikailo <john@ikailo.com>
2023-02-26 00:41:26 -0500

    Interim 0226

---
+++ Agent/Agent.cs
@@ -0,0 +1,45 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+
+namespace Technologai
+{
+    public class Agent
+    {
+        public event EventHandler<string>? Output;
+
+        private MqttClient _mqtt;
+
+        private const string USERNAME = "audiostream";
+        private const string PASSWORD = "7snLBemg1T";
+        private const int PORT = 1883;
+
+        private const string TOPIC = "my/topic";
+
+        public Agent(string host)
+        {
+            _mqtt = new MqttClient(host, PORT, USERNAME, PASSWORD);
+            _mqtt.MessageReceived += _mqtt_MessageReceived;
+        }
+
+        private void _mqtt_MessageReceived(object? sender, MqttApplicationMessageReceivedEventArgs args)
+        {   
+            Output?.Invoke(this, args.ApplicationMessage.ConvertPayloadToString());
+        }
+
+        public async void Publish(string message)
+        {
+            await _mqtt.PublishAsync(TOPIC, message);
+        }
+
+        public async void Start()
+        {
+            await _mqtt.ConnectAsync();
+            await _mqtt.SubscribeAsync(TOPIC);
+        }
+
+        public async void Stop()
+        {
+            await _mqtt.DisconnectAsync();
+        }
+    }
+}
---
+++ Agent/MqttClient.cs
@@ -0,0 +1,72 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+
+namespace Technologai
+{
+    internal class MqttClient
+    {
+        private string _host;
+        private int _port;
+        private string _username;
+        private string _password;        
+
+        private IMqttClient _client = new MqttFactory().CreateMqttClient();
+        private CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+        internal event EventHandler<MqttApplicationMessageReceivedEventArgs>? MessageReceived;        
+
+        internal MqttClient(string host, int port, string username, string password)
+        {
+            _host = host;
+            _port = port;
+            _username = username;
+            _password = password;            
+        }
+
+        internal async Task ConnectAsync()
+        {
+            if (!_client.IsConnected)
+            {
+                var options = new MqttClientOptionsBuilder()
+                .WithTcpServer(_host, _port)
+                .WithCredentials(_username, _password)
+                .Build();
+
+                _client.ApplicationMessageReceivedAsync += _client_ApplicationMessageReceivedAsync;
+
+                await _client.ConnectAsync(options, _cancellationTokenSource.Token);                             
+            }
+        }
+
+        internal async Task SubscribeAsync(string topic)
+        {
+            if (!_client.IsConnected) { throw new InvalidOperationException("Not Connected"); }
+
+            await _client.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic(topic).Build(), _cancellationTokenSource.Token);            
+        }
+
+        private async Task _client_ApplicationMessageReceivedAsync(MqttApplicationMessageReceivedEventArgs args)
+        {
+            await Task.Run(() => MessageReceived?.Invoke(this, args));
+        }
+
+        internal async Task DisconnectAsync()
+        {
+            _cancellationTokenSource.Cancel();
+            await _client.DisconnectAsync();
+            _client.Dispose();
+        }
+
+        internal async Task PublishAsync(string topic, string payload, bool retain = false, int qos = 0)
+        {
+            var message = new MqttApplicationMessageBuilder()
+                .WithTopic(topic)
+                .WithPayload(payload)
+                .WithRetainFlag(retain)
+                .WithQualityOfServiceLevel((MQTTnet.Protocol.MqttQualityOfServiceLevel)qos)
+                .Build();
+
+            await _client.PublishAsync(message, _cancellationTokenSource.Token);
+        }
+    }
+}
