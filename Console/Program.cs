43b5e7845132a90ab8895346db03ef1847c963c6
ikailo <github@ikailo.com>
2023-02-26 00:41:26 -0500

    Interim 0226

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,7 +1,49 @@
-﻿internal class Program
+﻿using Technologai;
+
+internal class Program
 {
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
+    private const string HOST = "44.209.26.208";
+    private static Agent _agent = new Agent(HOST);
+
+    internal static void Main(string[] args)
+    {   
+        _agent.Start();
+        _agent.Output += _agent_Output;
+
+        Run().Wait();
+
+        _agent.Stop();
     }
+
+    private static void _agent_Output(object? sender, string message)
+    {
+        Console.WriteLine($"output:> {message}");
+    }
+
+    private static void Input(string message)
+    {
+        _agent.Publish(message);
+    }
+
+    private async static Task Run()
+    {   
+        Console.WriteLine("input:>");
+        do
+        {
+            string value = await Task.Run(() =>
+            {
+                return Console.ReadLine() ?? "";
+            });
+
+            if (value.Equals("quit", StringComparison.OrdinalIgnoreCase)) { break; }
+
+            Input(value);            
+
+            Console.WriteLine($"sent:> {value}");
+        }
+        while (true);
+
+    }
+
+
 }
