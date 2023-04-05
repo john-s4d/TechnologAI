a82bed7b6b8055d835c8fce99faebb3f2e6429ef
ikailo <github@ikailo.com>
2023-04-04 22:40:56 -0400

    updates

--- Broker/aws/OpenID/Jwks.cs
+++ Broker/aws/OpenID/Jwks.cs
@@ -3,19 +3,14 @@ using Amazon.KeyManagementService;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Technologai.AWS.OpenID
 {
     internal class Jwks
     {
-        public async Task<APIGatewayHttpApiV2ProxyResponse> GetJwks(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+        public async Task<APIGatewayHttpApiV2ProxyResponse> JwksGet(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
         {
             GetPublicKeyRequest publicKeyRequest = new GetPublicKeyRequest
             {
@@ -28,18 +23,21 @@ namespace Technologai.AWS.OpenID
 
             rsaPublicKey.ImportSubjectPublicKeyInfo(publicKeyResponse.PublicKey.ToArray(), out int bytesRead);
 
-            var jwk = JsonWebKeyConverter.ConvertFromRSASecurityKey(new(rsaPublicKey)
-            {
-                KeyId = Config.SIGNATURE_KEY_ID
-            });
-
+            var jwk = JsonWebKeyConverter.ConvertFromRSASecurityKey(new(rsaPublicKey));
+            jwk.KeyId = Config.SIGNATURE_KEY_ID;                
             jwk.Use = "sig";
 
             return new APIGatewayHttpApiV2ProxyResponse
             {
                 StatusCode = 200,
-                Body = JsonExtensions.SerializeToJson(new JwksResponse { keys = { jwk } }) // JsonExtensions does a better job of serializing jwk
+                Body = JsonExtensions.SerializeToJson(new JwksResponse { keys = { jwk } })
             };
+        }
+
+        public class JwksResponse
+        {
+            public List<JsonWebKey> keys { get; set; } = new();
+
         }
     }
 }
