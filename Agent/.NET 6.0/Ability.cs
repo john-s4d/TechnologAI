6cbe7abedc2edc4a2594ccc820a91914421e9eca
ikailo <john@ikailo.com>
2023-04-14 00:41:13 -0400

    messages flowing better

--- Agent/.NET 6.0/Ability.cs
+++ Agent/.NET 6.0/Ability.cs
@@ -1,22 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
 
-namespace Technologai
+public class Ability
 {
-    public class Ability 
-    {
-        public string? Name { get; set; }
-        public string? Description { get; set; }        
-        public string? SchemaIn { get; set; }
-        public string? SchemaOut { get; set; }
-        public string? DoneWhen { get; set; }        
-        public string? MemberId { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public string? SampleJsonIn { get; set; }
+    public string? SampleJsonOut { get; set; }
+    public string? MemberId { get; set; }
 
-        //public string Serialize() { }
-        //public string Deserialize() { }
-        
-    }
 }
