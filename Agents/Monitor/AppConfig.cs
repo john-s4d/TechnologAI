b1f549f427abf464d93708f17027005aac497884
ikailo <john@ikailo.com>
2023-04-24 01:19:05 -0400

    monitor

---
+++ Agents/Monitor/AppConfig.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Technologai.Agents.Core.Debug
+{
+    internal class AppConfig
+    {
+        private readonly IConfiguration _config;
+
+        internal string? Authority => _config["authority"]; 
+        internal string? ClientSecret => _config["clientSecret"];
+        internal string? ClientId => _config["clientId"];
+        internal string? MemberId => _config["coordinatorMemberId"];
+
+        internal AppConfig()
+        {
+            _config = new ConfigurationBuilder()
+            .AddUserSecrets<AppConfig>()
+            .Build();
+        }
+    }
+}
---
+++ Agents/Monitor/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Technologai.Agents.Core.Debug
+{
+    internal class Program
+    {
+        private static Monitor? _monitor
+            ;
+
+        private static AppConfig _config = new AppConfig();
+
+        internal static async Task Main(string[] args)
+        {
+            var authorityName = _config.Authority ?? throw new ArgumentNullException(nameof(_config.Authority));
+            var clientId = _config.ClientId ?? throw new ArgumentNullException(nameof(_config.ClientId));
+            var clientSecret = _config.ClientSecret ?? throw new ArgumentNullException(nameof(_config.ClientSecret));
+            var memberId = _config.MemberId ?? throw new ArgumentNullException(nameof(_config.MemberId));
+
+            _monitor = new Monitor(authorityName, clientId, clientSecret, memberId);
+
+            _monitor.StatusMessage += _debug_StatusMessage;
+
+            Console.WriteLine("Loading...");
+
+            await _monitor.Start();
+            await Program.Run();
+            await _monitor.Stop();
+        }
+
+        private static void _debug_StatusMessage(object? sender, string message)        {
+
+            Console.WriteLine($"{_monitor?.Name ?? "Debug.Local"} {message}");            
+        }
+
+        private async static Task Run()
+        {
+            //Console.WriteLine($"{_debug?.Name} Started. \"quit\" to stop.");
+
+            do
+            {
+                //string value = await Task.Run(() =>
+                //{
+                //    return Console.ReadLine() ?? "";
+                //});
+
+                //if (value.Equals("quit", StringComparison.OrdinalIgnoreCase)) { break; }
+                                
+            }
+            while (true);
+
+        }
+
+    }
+
+
+
+}
