febc1749166a55961799d5bb87cc449524e4343b
ikailo <github@ikailo.com>
2023-04-24 01:19:05 -0400

    monitor

---
+++ Agents/Monitor/Monitor.cs
@@ -0,0 +1,11 @@
+﻿namespace Technologai.Agents.Core.Debug
+{
+    internal class Monitor : TechnologaiAgent
+    {
+        public Monitor(string authorityName, string clientId, string clientSecret, string memberId)
+            : base(authorityName, clientId, clientSecret, memberId)
+        {
+           
+        }
+    }
+}
