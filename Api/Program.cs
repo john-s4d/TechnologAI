15da5130e3ca3693f100ebdd6b0cf62443150ad8
qasim-aziz <envoyfacilitation@outlook.com>
2023-05-05 14:55:08 +0500

    added dockerfile, workflows

--- Api/Program.cs
+++ Api/Program.cs
@@ -12,9 +12,10 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
 
 app.UseHttpsRedirection();
 
